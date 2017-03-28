41581b86802eeecdd081c3aefe9549c2f782cdaa
Vincent Boutour <bob@vibioh.fr>
2017-03-28 21:53:25 +0200

    Migrating to ava

--- src/Fetch.test.js
+++ src/Fetch.test.js
@@ -1,380 +1,376 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-env mocha */
+import test from 'ava';
 import sinon from 'sinon';
-import { expect } from 'chai';
 import Fetch from '../src/Fetch';
 
-describe('Fetch', () => {
-  let stubFetch;
+let stubFetch;
 
-  const textPromise = Promise.resolve({
+const textPromise = Promise.resolve({
+  status: 200,
+  headers: {
+    get: () => 'text/plain',
+  },
+  text: () => Promise.resolve(),
+});
+
+const jsonPromise = Promise.resolve({
+  status: 200,
+  headers: {
+    get: () => 'application/json',
+  },
+  json: () => Promise.resolve(),
+});
+
+test.afterEach(() => {
+  if (stubFetch) {
+    stubFetch.restore();
+  }
+});
+
+test('should return a promise', (t) => {
+  global.fetch = () => Promise.resolve({
+    status: 200,
+    headers: {
+      get: () => '',
+    },
+    text: () => Promise.resolve(''),
+  });
+
+  const result = Fetch.get('/');
+
+  t.is(result instanceof Promise, true);
+});
+
+test('should return text when asked', (t) => {
+  global.fetch = () => Promise.resolve({
     status: 200,
     headers: {
       get: () => 'text/plain',
     },
-    text: () => Promise.resolve(),
+    text: () => Promise.resolve('Test Mocha'),
   });
 
-  const jsonPromise = Promise.resolve({
+  return Fetch.get('/')
+    .then(data => t.is(data, 'Test Mocha'));
+});
+
+test('should return json when asked', (t) => {
+  global.fetch = () => Promise.resolve({
     status: 200,
     headers: {
       get: () => 'application/json',
     },
-    json: () => Promise.resolve(),
+    json: () => Promise.resolve({
+      result: 'Test Mocha',
+    }),
   });
 
-  afterEach(() => {
-    if (stubFetch) {
-      stubFetch.restore();
-    }
+  return Fetch.get('/')
+    .then(data => t.deepEqual(data, {
+      result: 'Test Mocha',
+    }));
+});
+
+test('should return error when 400 or more', (t) => {
+  global.fetch = () => Promise.resolve({
+    status: 400,
+    headers: {
+      get: () => 'text/plain',
+    },
+    text: () => Promise.resolve('Test Mocha Error'),
   });
 
-  it('should return a promise', () => {
-    global.fetch = () => Promise.resolve({
-      status: 200,
-      headers: {
-        get: () => '',
-      },
-      text: () => Promise.resolve(''),
+  return Fetch.get('/')
+    .then(() => t.is(false, true))
+    .catch((data) => {
+      t.is(data.content, 'Test Mocha Error');
+      t.is(String(data), 'Test Mocha Error');
     });
+});
 
-    const result = Fetch.get('/');
-
-    expect(result).to.be.instanceof(Promise);
+test('should return jsonError when 400 or more', (t) => {
+  global.fetch = () => Promise.resolve({
+    status: 500,
+    headers: {
+      get: () => 'application/json',
+    },
+    json: () => Promise.resolve({
+      error: 'Test Mocha Error',
+    }),
   });
 
-  it('should return text when asked', () => {
-    global.fetch = () => Promise.resolve({
-      status: 200,
-      headers: {
-        get: () => 'text/plain',
-      },
-      text: () => Promise.resolve('Test Mocha'),
+  return Fetch.get('/')
+    .then(() => t.is(false, true))
+    .catch((data) => {
+      t.deepEqual(data.content, { error: 'Test Mocha Error' });
+      t.is(String(data), '{"error":"Test Mocha Error"}');
     });
+});
 
-    return Fetch.get('/')
-      .then(data => expect(data).to.eql('Test Mocha'));
+test('should return error when text fail', (t) => {
+  global.fetch = () => Promise.resolve({
+    status: 200,
+    headers: {
+      get: () => 'text/plain',
+    },
+    text: () => Promise.reject(new Error('Mocha Text Error')),
   });
 
-  it('should return json when asked', () => {
-    global.fetch = () => Promise.resolve({
-      status: 200,
+  return Fetch.get('/')
+    .then(() => t.is(false, true))
+    .catch(data => t.is(String(data), 'Error: Mocha Text Error'));
+});
+
+test('should return error when json fail', (t) => {
+  global.fetch = () => Promise.resolve({
+    status: 200,
+    headers: {
+      get: () => 'application/json',
+    },
+    json: () => Promise.reject(new Error('Mocha JSON Error')),
+  });
+
+  return Fetch.get('/')
+    .then(() => t.is(false, true))
+    .catch(data => t.is(String(data), 'Error: Mocha JSON Error'));
+});
+
+test.serial('should pass header', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+
+  return Fetch.url('/').header('custom', 'test').get()
+    .then(() => t.is(stubFetch.calledWith('/', {
       headers: {
-        get: () => 'application/json',
+        custom: 'test',
       },
-      json: () => Promise.resolve({
-        result: 'Test Mocha',
-      }),
-    });
+      method: 'GET',
+    }), true));
+});
 
-    return Fetch.get('/')
-      .then(data => expect(data).to.eql({
-        result: 'Test Mocha',
-      }));
-  });
+test.serial('should pass auth', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
 
-  it('should return error when 400 or more', () => {
-    global.fetch = () => Promise.resolve({
-      status: 400,
+  return Fetch.url('/').auth('token').get()
+    .then(() => t.is(stubFetch.calledWith('/', {
       headers: {
-        get: () => 'text/plain',
+        Authorization: 'token',
       },
-      text: () => Promise.resolve('Test Mocha Error'),
-    });
+      method: 'GET',
+    }), true));
+});
 
-    return Fetch.get('/')
-      .then(() => expect(false).to.be.true)
-      .catch((data) => {
-        expect(data.content).to.eql('Test Mocha Error');
-        expect(String(data)).to.eql('Test Mocha Error');
-      });
-  });
+test.serial('should pass contentType for json', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
 
-  it('should return jsonError when 400 or more', () => {
-    global.fetch = () => Promise.resolve({
-      status: 500,
+  return Fetch.url('/').contentJson().get()
+    .then(() => t.is(stubFetch.calledWith('/', {
       headers: {
-        get: () => 'application/json',
+        'Content-Type': 'application/json',
       },
-      json: () => Promise.resolve({
-        error: 'Test Mocha Error',
-      }),
-    });
+      method: 'GET',
+    }), true));
+});
 
-    return Fetch.get('/')
-      .then(() => expect(false).to.be.true)
-      .catch((data) => {
-        expect(data.content).to.eql({ error: 'Test Mocha Error' });
-        expect(String(data)).to.eql('{"error":"Test Mocha Error"}');
-      });
-  });
+test.serial('should pass contentType for text', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
 
-  it('should return error when text fail', () => {
-    global.fetch = () => Promise.resolve({
-      status: 200,
+  return Fetch.url('/').contentText().get()
+    .then(() => t.is(stubFetch.calledWith('/', {
       headers: {
-        get: () => 'text/plain',
+        'Content-Type': 'text/plain',
       },
-      text: () => Promise.reject(new Error('Mocha Text Error')),
-    });
+      method: 'GET',
+    }), true));
+});
 
-    return Fetch.get('/')
-      .then(() => expect(false).to.be.true)
-      .catch(data => expect(String(data)).to.eql('Error: Mocha Text Error'));
-  });
+test.serial('should pass accept for json', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
 
-  it('should return error when json fail', () => {
-    global.fetch = () => Promise.resolve({
-      status: 200,
+  return Fetch.url('/').acceptJson().get()
+    .then(() => t.is(stubFetch.calledWith('/', {
       headers: {
-        get: () => 'application/json',
+        Accept: 'application/json',
       },
-      json: () => Promise.reject(new Error('Mocha JSON Error')),
-    });
+      method: 'GET',
+    }), true));
+});
 
-    return Fetch.get('/')
-      .then(() => expect(false).to.be.true)
-      .catch(data => expect(String(data)).to.eql('Error: Mocha JSON Error'));
+test.serial('should pass accept for text', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+
+  return Fetch.url('/').acceptText().get()
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {
+        Accept: 'text/plain',
+      },
+      method: 'GET',
+    }), true));
+});
+
+test.serial('should pass body', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+
+  return Fetch.url('/').body('Hello World').post()
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: 'Hello World',
+      method: 'POST',
+    }), true));
+});
+
+test.serial('should pass json body', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
+
+  return Fetch.url('/').body(JSON.stringify({ hello: 'World' })).post()
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: '{"hello":"World"}',
+      method: 'POST',
+    }), true));
+});
+
+test.serial('should not pass body if undefined', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+
+  return Fetch.url('/').body().post()
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {},
+      method: 'POST',
+    }), true));
+});
+
+test.serial('should guess json content', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
+
+  return Fetch.post('/', JSON.stringify({ hello: 'World' }))
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: '{"hello":"World"}',
+      method: 'POST',
+    }), true));
+});
+
+test.serial('should guess text content', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+
+  return Fetch.post('/', '{hello":"World"}')
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: '{hello":"World"}',
+      method: 'POST',
+    }), true));
+});
+
+test.serial('should not guess content if already defined', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+
+  return Fetch.url('/').contentText().body('{"hello":"World"}').post()
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: '{"hello":"World"}',
+      method: 'POST',
+    }), true));
+});
+
+test.serial('should not guess content if forbidden', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+
+  return Fetch.url('/').body('{"hello":"World"}', false).post()
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {},
+      body: '{"hello":"World"}',
+      method: 'POST',
+    }), true));
+});
+
+test('should use given content handler', (t) => {
+  global.fetch = () => Promise.resolve({
+    status: 204,
   });
 
-  it('should pass header', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+  let result;
+  const customContentHandler = content => (result = content);
 
-    return Fetch.url('/').header('custom', 'test').get()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          custom: 'test',
-        },
-        method: 'GET',
-      })).to.be.true);
-  });
-
-  it('should pass auth', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.url('/').auth('token').get()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          Authorization: 'token',
-        },
-        method: 'GET',
-      })).to.be.true);
-  });
-
-  it('should pass contentType for json', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
-
-    return Fetch.url('/').contentJson().get()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method: 'GET',
-      })).to.be.true);
-  });
-
-  it('should pass contentType for text', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.url('/').contentText().get()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        method: 'GET',
-      })).to.be.true);
-  });
-
-  it('should pass accept for json', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
-
-    return Fetch.url('/').acceptJson().get()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          Accept: 'application/json',
-        },
-        method: 'GET',
-      })).to.be.true);
-  });
-
-  it('should pass accept for text', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.url('/').acceptText().get()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          Accept: 'text/plain',
-        },
-        method: 'GET',
-      })).to.be.true);
-  });
-
-  it('should pass body', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.url('/').body('Hello World').post()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        body: 'Hello World',
-        method: 'POST',
-      })).to.be.true);
-  });
-
-  it('should pass json body', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
-
-    return Fetch.url('/').body(JSON.stringify({ hello: 'World' })).post()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: '{"hello":"World"}',
-        method: 'POST',
-      })).to.be.true);
-  });
-
-  it('should not pass body if undefined', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.url('/').body().post()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {},
-        method: 'POST',
-      })).to.be.true);
-  });
-
-  it('should guess json content', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => jsonPromise);
-
-    return Fetch.post('/', JSON.stringify({ hello: 'World' }))
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: '{"hello":"World"}',
-        method: 'POST',
-      })).to.be.true);
-  });
-
-  it('should guess text content', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.post('/', '{hello":"World"}')
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        body: '{hello":"World"}',
-        method: 'POST',
-      })).to.be.true);
-  });
-
-  it('should not guess content if already defined', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.url('/').contentText().body('{"hello":"World"}').post()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        body: '{"hello":"World"}',
-        method: 'POST',
-      })).to.be.true);
-  });
-
-  it('should not guess content if forbidden', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.url('/').body('{"hello":"World"}', false).post()
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {},
-        body: '{"hello":"World"}',
-        method: 'POST',
-      })).to.be.true);
-  });
-
-  it('should use given content handler', () => {
-    global.fetch = () => Promise.resolve({
+  return Fetch.url('/').content(customContentHandler).get().then(() => {
+    t.deepEqual(result, {
       status: 204,
     });
+  });
+});
 
-    let result;
-    const customContentHandler = content => (result = content);
-
-    return Fetch.url('/').content(customContentHandler).get().then(() => {
-      expect(result).to.be.eql({
-        status: 204,
-      });
-    });
+test('should use given error handler', (t) => {
+  global.fetch = () => Promise.resolve({
+    status: 404,
+    reason: 'Failed',
   });
 
-  it('should use given error handler', () => {
-    global.fetch = () => Promise.resolve({
-      status: 404,
-      reason: 'Failed',
-    });
+  let result;
+  const customErrorHandler = (content) => {
+    result = content.reason;
 
-    let result;
-    const customErrorHandler = (content) => {
-      result = content.reason;
+    return Promise.reject();
+  };
 
-      return Promise.reject();
-    };
+  return Fetch.url('/').error(customErrorHandler).get()
+    .then(() => t.is(false, true))
+    .catch(() => t.is(result, 'Failed'));
+});
 
-    return Fetch.url('/').error(customErrorHandler).get()
-      .then(() => expect(false).to.be.true)
-      .catch(() => expect(result).to.be.eql('Failed'));
-  });
+test.serial('should send GET', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
 
-  it('should send GET', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+  return Fetch.get('/')
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {},
+      method: 'GET',
+    }), true));
+});
 
-    return Fetch.get('/')
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {},
-        method: 'GET',
-      })).to.be.true);
-  });
+test.serial('should send POST', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
 
-  it('should send POST', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+  return Fetch.post('/')
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {},
+      method: 'POST',
+    }), true));
+});
 
-    return Fetch.post('/')
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {},
-        method: 'POST',
-      })).to.be.true);
-  });
+test.serial('should send PUT', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
 
-  it('should send PUT', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+  return Fetch.put('/')
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {},
+      method: 'PUT',
+    }), true));
+});
 
-    return Fetch.put('/')
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {},
-        method: 'PUT',
-      })).to.be.true);
-  });
+test.serial('should send PATCH', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
 
-  it('should send PATCH', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
+  return Fetch.patch('/')
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {},
+      method: 'PATCH',
+    }), true));
+});
 
-    return Fetch.patch('/')
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {},
-        method: 'PATCH',
-      })).to.be.true);
-  });
+test.serial('should send DELETE', (t) => {
+  stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
 
-  it('should send DELETE', () => {
-    stubFetch = sinon.stub(global, 'fetch').callsFake(() => textPromise);
-
-    return Fetch.delete('/')
-      .then(() => expect(stubFetch.calledWith('/', {
-        headers: {},
-        method: 'DELETE',
-      })).to.be.true);
-  });
+  return Fetch.delete('/')
+    .then(() => t.is(stubFetch.calledWith('/', {
+      headers: {},
+      method: 'DELETE',
+    }), true));
 });
